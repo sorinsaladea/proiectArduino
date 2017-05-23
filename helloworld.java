9a0ff8e52a7ce9974643772ab35b4df99e0f7a9f
sorinsaladea <sorinsaladea@yahoo.com>
2017-05-23 08:29:57 +0300

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -16,7 +16,7 @@
  *
  ******************************************************************************/
 
-public class HelloWorld {
+public class helloworld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
